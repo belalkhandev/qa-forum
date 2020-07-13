78dffb6be30b42f8eaf5afffbee718ce2694047a
Belal Khan <belalkhan.dev@gmail.com>
2020-07-13 08:34:50 +0600

    slider & notification database table creatye

--- database/migrations/2020_07_13_075404_create_notifications_table.php
+++ database/migrations/2020_07_13_075404_create_notifications_table.php
@@ -15,6 +15,12 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedSmallInteger('notification_from_user_id');
+            $table->unsignedSmallInteger('notification_to_user_id')->nullable();
+            $table->unsignedSmallInteger('notification_for_id');
+            $table->string('notification_for');
+            $table->string('notification_title');
+            $table->boolean('seen')->default(false);
             $table->timestamps();
         });
     }
