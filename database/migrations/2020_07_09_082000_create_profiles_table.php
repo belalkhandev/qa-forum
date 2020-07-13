55dd553e1d76806f44b71b2d5ab81ab537541708
Belal Khan <belalkhan.dev@gmail.com>
2020-07-13 13:14:31 +0600

    modify profile table

--- database/migrations/2020_07_09_082000_create_profiles_table.php
+++ database/migrations/2020_07_09_082000_create_profiles_table.php
@@ -18,6 +18,13 @@ class CreateProfilesTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('first_name');
             $table->string('last_name');
+            $table->string('district')->nullable();
+            $table->string('address')->nullable();
+            $table->string('qualification')->nullable();
+            $table->string('website')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('youtube')->nullable();
+            $table->string('linkedin')->nullable();
             $table->string('photo')->nullable();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
